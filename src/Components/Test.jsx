02736546eaa6b831124a5a7ef8027bf0f8eb9990
Shantanu <shantanugujar3807@gmail.com>
2025-09-17 11:35:04 +0530

    first

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -5,8 +5,8 @@ const Test = () => {
 
     return (
         <>
-            <h1>Hello</h1>
-            <h1>hii</h1>
+            <h1>You are using a functional component with arrow syntax</h1>
+            <h1>Your React component syntax is almost correct</h1>
 
         </>
     )
