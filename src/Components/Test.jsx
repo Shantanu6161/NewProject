7251ac3dabbfe606b980d9c9aa7e005d372f91d4
Shantanu <shantanugujar3807@gmail.com>
2025-09-17 11:54:42 +0530

    change

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -7,7 +7,7 @@ const Test = () => {
         <>
             <h1>You are using a functional component with arrow syntax</h1>
             <h1>Your React component syntax is almost correct</h1>
-            <div>nothing</div>
+            <div>This is first page</div>
 
         </>
     )
