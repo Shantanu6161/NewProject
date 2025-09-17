82b117d4713813310f51eae11732c5afb7e2b549
Shantanu <shantanugujar3807@gmail.com>
2025-09-17 13:14:45 +0530

    completed

--- src/Components/Test.jsx
+++ src/Components/Test.jsx
@@ -8,7 +8,6 @@ const Test = () => {
             <div>This is branch </div>
             <div>how to confirm this is branch</div>
             <div>confirm, this are branch</div>
-            <h1>change</h1>
         </>
     )
 };
